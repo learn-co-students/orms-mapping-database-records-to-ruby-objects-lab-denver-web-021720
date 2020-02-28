7788b1a3e6cf9cc662ffdfba8d5f128d7856f45e
joegelay <joegelay@gmail.com>
2020-02-28 11:07:03 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,43 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new  # self.new is the same as running Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
-  end
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
+  end 
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE name = ?
+      LIMIT 1 
+    SQL
+
+      DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+      end.first 
   end
   
   def save
@@ -40,4 +65,73 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
-end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+      end 
+  end 
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+      end 
+  end
+
+
+  def self.first_X_students_in_grade_10(num) 
+  # This is a class method that takes in an argument of the number of students from grade 10 to select. 
+  # This method should return an array of exactly X number of students.
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+      end 
+
+  end 
+
+  def self.first_student_in_grade_10 
+  # This is a class method that does not need an argument. 
+  # This should return the first student that is in grade 10.
+    sql = <<-SQL
+        SELECT *
+        FROM students 
+        WHERE grade = 10
+      SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+      end.first
+  end 
+
+  def self.all_students_in_grade_X(grade)
+  # This is a class method that takes in an argument of a grade for which to retrieve the roster. 
+  # This method should return an array of all students for grade X.
+  sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+      end 
+  end
+end 
